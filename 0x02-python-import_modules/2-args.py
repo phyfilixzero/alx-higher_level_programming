3e86f006dd419d924eecb9203c9a250ed321ce47
phyfilixzero <felixuc2580@gmail.com>
2022-06-02 21:05:58 +0100

    correction 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-
-if __name == "__main__":
-    import sys
-    argv = sys.argv[1:]
-    arg_num = len(argv)
+from sys import argv
+if __name__ == "__main__":
     i = 1
-    if arg_num == 0:
-        print("{:d} arguments.".format(arg_num))
-    elif arg_num == 1:
-        print("{:d} argument.".format(arg_num))
-        print("{:d}: {:s}".format(i, sys.argv[1]))
+    if len(argv) == 1:
+        print("{:d} arguments.".format(len(argv) - 1))
+    elif len(argv) == 2:
+        print("{:d} argument:".format(len(argv) - 1))
+        print("{:d}: {:s}".format(i, argv[i]))
+
     else:
-        print("{:d} arguments.".format(arg_num))
-        while i < arg_num:
-            print("{:d}: {:s}".format(i, sys.argv[i]))
-            i += 1
+        print("{:d} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{:d}: {:s}".format(i, argv[i]))
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -1,1 +1,13 @@
-This directory contains Python scripts that cover module importation and implementation as executed in Python 3.4.
+# 0x02. Python - import & modules
+
+0. imports function and prints result of addition
+
+1. import function and prints results of calculations
+
+2. prints number of arguments and the argument list
+
+3. prints the result of the addition of all arguments
+
+4. prints all the names defined by a specific compiled module
+
+5. imports variable from file and prints its value
