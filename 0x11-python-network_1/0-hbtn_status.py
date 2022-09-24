3e9fb28be43608914c57e53b6f849b4aac61022b
uche felix <felixuc2580@gmail.com>
2022-09-24 14:06:05 +0100

    edit 1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,6 @@ fetches https://intranet.hbtn.io/status.
 
 
 if __name__ == '__main__':
-    import urllib.request
 
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
         content = resp.read()
