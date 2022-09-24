de0f4afd426ddeb4519dc14e43893761c7866258
uche felix <felixuc2580@gmail.com>
2022-09-24 14:21:50 +0100

    edit 3

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" Script that fetches from a url using urllib package"""
+"""urllib usage"""
+import urllib.request
+import urllib.parse
 
-import urllib.request as request
 
 if __name__ == "__main__":
-    with request.urlopen('https://intranet.hbtn.io/status') as r:
-        html = r.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type:', type(html))
+        print('\t- content:', html)
+        print('\t- utf8 content:', html.decode('UTF-8'))
--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-""" Uses requests module to fetch URL status"""
-
+"""Python script that fetches https://intranet.hbtn.io/status
+"""
 import requests
 
-if __name__ == '__main__':
-    response = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-          .format(type(response.text), response.text))
+
+if __name__ == "__main__":
+    r = requests.get('https://intranet.hbtn.io/status')
+    print('Body response:')
+    print('\t- type:', type(r.text))
+    print('\t- content:', r.text)
