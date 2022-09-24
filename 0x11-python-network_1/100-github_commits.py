2b0d5859bab21926ad4c58376adea80efc073ab0
uche felix <felixuc2580@gmail.com>
2022-09-24 14:19:27 +0100

    commit challenge

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-""" Github commits code challenge"""
-
-import requests
+"""
+git commit challenge
+"""
 from sys import argv
+import requests
 
-if __name__ == '__main__':
-    url = "https://api.github.com/repos/{}/{}commits"\
-          .format(argv[2], argv[1])
-    r = requests.get(url)
-    commits = r.json()
 
-    for commit in commits[:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+if __name__ == "__main__":
+    r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                     .format(argv[2], argv[1]))
+    l = r.json()
+    try:
+        for i in range(100):
+            print(l[i].get('sha'), l[i].get('commit')
+                  .get('author').get('name'), sep=": ")
+    except:
+        pass
